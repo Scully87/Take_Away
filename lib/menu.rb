25a460ad99c1218f99dfc89ca01779041cc5748a
Scully87 <scully87.ts@googlemail.com>
2014-09-27 23:55:13 +0100

    added menu initialization

---
+++ lib/menu.rb
@@ -0,0 +1,18 @@
+class Menu
+
+attr_accessor :dishes
+
+	def initialize
+		@dishes = []
+	end
+
+	def add_dish(dish)
+		dishes << dish
+	end
+
+	def list_dishes
+			@dishes.each { |dish| dish.to_s } 
+	end
+
+end
+	
---
+++ spec/menu_spec.rb
@@ -0,0 +1,22 @@
+require 'menu'
+
+describe 'Menu' do
+	let(:menu) {Menu.new}
+	let(:pizza) {double :dish, :name => "Pizza", :price => 8.00}
+
+	context "Initialization" do
+		it "should list an empty dishes array" do
+			expect(menu.dishes).to eq([])
+		end
+	end
+
+	context "does" do
+		it "add a dish" do
+			expect(menu.add_dish(pizza)).to eq([pizza])
+		end
+		it "list the dishes and prices" do 
+			menu.add_dish(pizza)
+			menu.list_dishes
+		end
+	end
+end
