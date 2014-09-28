2457a43bfe2ee57a12821c19c1860a55e4bfed6f
Scully87 <scully87.ts@googlemail.com>
2014-09-28 16:29:49 +0100

    order spec first test

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,8 +8,8 @@ require 'lineitem'
     let(:pizza) { Dish.new("Pizza", 8.50) }
 
     it "adds lineitems list to final order" do
-      order.add(LineItem.new(pizza, 3))
-      expect(order.line_items.count).to eq 1
+        order.add(LineItem.new(pizza, 3))
+        expect(order.line_items.count).to eq 1
     end
 
     
