3b0b9d10cfeae156c31080f42545cae978c86df3
Scully87 <scully87.ts@googlemail.com>
2014-09-29 09:33:01 +0100

    twilio sends sms, gives arrival time message +0100

--- lib/send.rb
+++ lib/send.rb
@@ -1,12 +1,20 @@
-# require 'rubygems'
-# require 'twilio-ruby'
+require 'rubygems'
+require 'twilio-ruby'
+ 
+#def send
 
-# class Send
+	account_sid = 'ACfc5faa1825f422c6798ea3e6d28f8257'
+	auth_token = '31680b024f874b322843b6aa7ea46cc3'
 
-# 	attr_reader :account_sid, :auth_token
+	@client = Twilio::REST::Client.new account_sid, auth_token
 
-# 		def initialize
-# 			@account_sid = "ACfc5faa1825f422c6798ea3e6d28f8257"
-# 			@auth_token = "31680b024f874b322843b6aa7ea46cc3"
-# 		end
-# end
+ 	#time = Time.now
+ 	time60 = (Time.now + (60*60)).strftime("at %I:%M%p")
+
+	@client.account.messages.create({
+		:to => "+447884496850",     
+    	:from => "+441233800175",  
+   		:body => "Thank you, your order will arrive before #{time60} you fat twat!!!!" 
+    })
+
+#end
