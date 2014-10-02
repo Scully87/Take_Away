3eef67ef268ced4bda8c74fc2fdfcb7508cdc3ad
Scully87 <scully87.ts@googlemail.com>
2014-10-02 17:45:43 +0100

    fixed readme

--- lib/send.rb
+++ lib/send.rb
@@ -13,14 +13,14 @@ class Send
 	def initialize
     @time
   	@account_sid = 'ACfc5faa1825f422c6798ea3e6d28f8257'
-  	@auth_token = '31680b024f874b322843b6aa7ea46cc3'
-  	@from = '+441233800175'
-  	@to = '+447884496850'
-    @client = Twilio::REST::Client.new @account_sid, @auth_token
+  	@auth_token =  '31680b024f874b322843b6aa7ea46cc3'
+  	@from =        '+441233800175'
+  	@to =          '+447884496850'
+    @client =       Twilio::REST::Client.new @account_sid, @auth_token
   end
 
   def message(time = Time.now.strftime("at %I:%M%p"))
-  	@message = "Thank you, your order will arrive before #{time} you fatty!!!!"
+  	@message = "Thank you, your order will arrive before #{time}"
   end
 
 end
--- spec/send_spec.rb
+++ spec/send_spec.rb
@@ -9,16 +9,14 @@ describe Send do
     Timecop.freeze(new_time)
   end
 
-  let(:send) { Send.new }
+  let(:send)        { Send.new }
   let(:account_sid) { 'ACfc5faa1825f422c6798ea3e6d28f8257' }
-  let(:auth_token) { '31680b024f874b322843b6aa7ea46cc3' }
-  let(:from) { '+441233800175' }
-  let(:to) { '+447884496850'}
-  let(:time) { Time.now.strftime("at %I:%M%p") }
-  let(:message) { "Thank you, your order will arrive before #{time} you fatty!!!!" }
-  let(:body) {'my_message'}
-
-	
+  let(:auth_token)  { '31680b024f874b322843b6aa7ea46cc3' }
+  let(:from)        { '+441233800175' }
+  let(:to)          { '+447884496850'}
+  let(:time)        { Time.now.strftime("at %I:%M%p") }
+  let(:message)     { "Thank you, your order will arrive before #{time}" }
+  	
   it "should have an account_sid" do
     expect(send.account_sid).to eq(account_sid)
   end
